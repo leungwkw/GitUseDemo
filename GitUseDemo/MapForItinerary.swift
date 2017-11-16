1c11c83adff5daf778ff71c2c9e9ec4938ae164b
William Leung <williamleung@wais-imac.resnet.wustl.edu>
2017-11-16 12:01:47 -0600

    Created MapForItinerary file.

---
+++ GitUseDemo/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitUseDemo
+//
+//  Created by William Leung on 11/16/17.
+//  Copyright © 2017 Wai Kiet William Leung. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+
+}
